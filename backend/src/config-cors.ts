f178aef0de9830fce9b1a95bde6430deedbb1e99
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2020-10-10 06:44:55 +0000

    ui and functionality

--- backend/src/config-cors.ts
+++ backend/src/config-cors.ts
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3300",
   "http://localhost:3000",
   "https://endsarsjourney-jawcqqbhs.vercel.app/",
+  "https://endsarsjourney.vercel.app/",
 ];
 
 export const corsOption = {
